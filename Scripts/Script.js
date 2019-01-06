97b7464764f830d059d7bd098e0767d97e64a71c
cuongbetu <cuongbetu@gmail.com>
2019-01-06 11:10:21 +0700

    upload code clone babi.vn

---
+++ Scripts/Script.js
@@ -0,0 +1,18 @@
+﻿$(function () {
+    $("#contact .address").hide();
+    $("#contact .system").click(function () {
+        $("#contact .address").toggle();
+    });
+    $(".bar1").click(function () {
+        $("#menu .menu1").toggle();
+    });
+    $("header .account ").click(function () {
+        $("header .account .account-form").toggle();
+    });
+    $(".account .account-form .signin").click(function() {
+        $(".account-signin").show();
+    });
+    $(".account-signin .close-form").click(function() {
+        $(".account-signin").hide();
+    });
+});
